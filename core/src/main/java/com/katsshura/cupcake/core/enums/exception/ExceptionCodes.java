6b63e89ac8e2f1fcff0df86f1f369242e96384bb
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2022-09-20 23:23:48 -0300

    Created new exception

--- core/src/main/java/com/katsshura/cupcake/core/enums/exception/ExceptionCodes.java
+++ core/src/main/java/com/katsshura/cupcake/core/enums/exception/ExceptionCodes.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionCodes {
-    ALREADY_EXISTS("The resource [%s] already exists in our server!");
+    ALREADY_EXISTS("The resource [%s] already exists in our server!"),
+    INCOMPATIBLE_VALUE("The value [%s} is incompatible!");
 
     private final String message;
 
