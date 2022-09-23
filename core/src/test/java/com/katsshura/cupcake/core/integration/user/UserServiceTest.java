37b4139ba42bcc4b2d56136f100c7f6b44fa5951
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2022-09-22 22:42:12 -0300

    Implemented first test for user integration

--- core/src/test/java/com/katsshura/cupcake/core/integration/user/UserServiceTest.java
+++ core/src/test/java/com/katsshura/cupcake/core/integration/user/UserServiceTest.java
@@ -2,15 +2,20 @@ package com.katsshura.cupcake.core.integration.user;
 
 
 import com.katsshura.cupcake.core.config.IntegrationTestsConfiguration;
+import com.katsshura.cupcake.core.entities.address.AddressEntity;
+import com.katsshura.cupcake.core.entities.user.UserEntity;
 import com.katsshura.cupcake.core.repositories.user.UserRepository;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @IntegrationTestsConfiguration
 public class UserServiceTest {
@@ -18,21 +23,39 @@ public class UserServiceTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    public void contextLoads() {
-        assertAll(
-                () -> assertThat(this.userRepository, is(notNullValue()))
-        );
-    }
+
 
     @Nested
     @IntegrationTestsConfiguration
     class CreateUser {
 
+        @Test
         void shouldCreateUserWithSuccess() {
+            final var address = AddressEntity.builder()
+                    .cep("76804066")
+                    .propertyNumber("10")
+                    .logradouro("Rua Joaquim Nabuco")
+                    .build();
+            final var user = UserEntity.builder()
+                    .cpf("05155638038")
+                    .birthdayDate(LocalDate.parse("1999-01-20"))
+                    .email("test1@test.com")
+                    .name("Test One")
+                    .password("somepassword")
+                    .registeredAddresses(List.of(address))
+                    .build();
+
+            final var result = userRepository.save(user);
+
+            assertAll(
+                    () -> assertNotNull(result),
+                    () -> assertThat(result.getId(), allOf(greaterThan(0L), notNullValue())),
+                    () -> assertEquals(user, result)
+            );
 
         }
 
+        @Test
         void shouldThrowErrorWhenTryingSaveUserInvalidData() {
 
         }
