3da73c9ad235adcdee36bb1e36e7e45dd8cc2440
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2022-09-25 00:23:09 -0300

    Implemented user repository tests

--- core/src/test/java/com/katsshura/cupcake/core/integration/user/UserServiceTest.java
+++ core/src/test/java/com/katsshura/cupcake/core/integration/user/UserServiceTest.java
@@ -7,9 +7,14 @@ import com.katsshura.cupcake.core.entities.user.UserEntity;
 import com.katsshura.cupcake.core.repositories.user.UserRepository;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -79,21 +84,96 @@ public class UserServiceTest {
     @Nested
     @IntegrationTestsConfiguration
     class ListUser {
+        @ParameterizedTest(name = "#[{index}] Should assertNotNull and assertEquals for all entity properties when" +
+                " find by email with parameters values: Email = {0} | Name = {1} | CPF = {2} | Password = {3} | CEP = {4}")
+        @CsvFileSource(resources = "/csv/user/UserRepositoryValidData.csv", numLinesToSkip = 1)
+        @SqlGroup({
+                @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+                        scripts = "classpath:scripts/user/BeforeUserRepositoryTest.sql"),
+                @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+                        scripts = "classpath:scripts/user/AfterUserRepositoryTest.sql")
+        })
+        @Transactional
+        void shouldFindUserByEmail(final String name, final String email, final String cpf,
+                                   final String password, final String cep) {
 
-        void shouldFindUserByEmail() {
+            final var result = userRepository.findByEmail(email);
 
+            assertAll(
+                    () -> assertFalse(result.isEmpty()),
+                    () -> assertEquals(email, result.get().getEmail()),
+                    () -> assertEquals(name, result.get().getName()),
+                    () -> assertEquals(cpf, result.get().getCpf()),
+                    () -> assertEquals(password, result.get().getPassword()),
+                    () -> assertEquals(cep, result.get().getRegisteredAddresses().get(0).getCep())
+            );
         }
 
-        void shouldFindUserByCpf() {
+        @ParameterizedTest(name = "#[{index}] Should assertNotNull and assertEquals for all entity properties when" +
+                " find by cpf with parameters values: Email = {0} | Name = {1} | CPF = {2} | Password = {3} | CEP = {4}")
+        @CsvFileSource(resources = "/csv/user/UserRepositoryValidData.csv", numLinesToSkip = 1)
+        @SqlGroup({
+                @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+                        scripts = "classpath:scripts/user/BeforeUserRepositoryTest.sql"),
+                @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+                        scripts = "classpath:scripts/user/AfterUserRepositoryTest.sql")
+        })
+        @Transactional
+        void shouldFindUserByCpf(final String name, final String email, final String cpf,
+                                 final String password, final String cep) {
 
+            final var result = userRepository.findByCpf(cpf);
+
+            assertAll(
+                    () -> assertFalse(result.isEmpty()),
+                    () -> assertEquals(email, result.get().getEmail()),
+                    () -> assertEquals(name, result.get().getName()),
+                    () -> assertEquals(cpf, result.get().getCpf()),
+                    () -> assertEquals(password, result.get().getPassword()),
+                    () -> assertEquals(cep, result.get().getRegisteredAddresses().get(0).getCep())
+            );
         }
 
-        void shouldFindUserByEmailOrCpf() {
+        @ParameterizedTest(name = "#[{index}] Should assertNotNull and assertEquals for all entity properties when" +
+                " find by email or cpf with parameters values: Email = {0} | Name = {1} | CPF = {2} | Password = {3} | CEP = {4}")
+        @CsvFileSource(resources = "/csv/user/UserRepositoryValidData.csv", numLinesToSkip = 1)
+        @SqlGroup({
+                @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+                        scripts = "classpath:scripts/user/BeforeUserRepositoryTest.sql"),
+                @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+                        scripts = "classpath:scripts/user/AfterUserRepositoryTest.sql")
+        })
+        @Transactional
+        void shouldFindUserByEmailOrCpf(final String name, final String email, final String cpf,
+                                        final String password, final String cep) {
 
+            final var result = userRepository.findByEmailOrCpf(email, cpf);
+
+            assertAll(
+                    () -> assertFalse(result.isEmpty()),
+                    () -> assertEquals(email, result.get().getEmail()),
+                    () -> assertEquals(name, result.get().getName()),
+                    () -> assertEquals(cpf, result.get().getCpf()),
+                    () -> assertEquals(password, result.get().getPassword()),
+                    () -> assertEquals(cep, result.get().getRegisteredAddresses().get(0).getCep())
+            );
         }
 
-        void shouldValidateUserByEmailOrCpf() {
+        @ParameterizedTest(name = "#[{index}] Should assertTrue for all entity properties when" +
+                " validating by email or cpf with parameters values: Email = {0} | Name = {1} | CPF = {2} |" +
+                " Password = {3} | CEP = {4}")
+        @CsvFileSource(resources = "/csv/user/UserRepositoryValidData.csv", numLinesToSkip = 1)
+        @SqlGroup({
+                @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+                        scripts = "classpath:scripts/user/BeforeUserRepositoryTest.sql"),
+                @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+                        scripts = "classpath:scripts/user/AfterUserRepositoryTest.sql")
+        })
+        @Transactional
+        void shouldValidateUserByEmailOrCpf(final String name, final String email, final String cpf) {
 
+            final var result = userRepository.existsByEmailOrCpf(email, cpf);
+            assertTrue(result);
         }
     }
 }
