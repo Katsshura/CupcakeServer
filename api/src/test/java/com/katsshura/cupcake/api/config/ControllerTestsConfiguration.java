da24ccec713de994b551e53eecf234028388a752
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2022-09-25 22:34:30 -0300

    Implemented config files for API tests

---
+++ api/src/test/java/com/katsshura/cupcake/api/config/ControllerTestsConfiguration.java
@@ -0,0 +1,17 @@
+package com.katsshura.cupcake.api.config;
+
+import com.katsshura.cupcake.api.ApiApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@SpringBootTest(classes = ApiApplication.class)
+@AutoConfigureMockMvc(addFilters = false)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ControllerTestsConfiguration {
+}
