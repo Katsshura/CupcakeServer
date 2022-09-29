d636fda19176a970b255abf9857389fb4885b8c7
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2022-09-28 23:24:21 -0300

    Implemented endpoint to get product by id or search

--- api/src/main/java/com/katsshura/cupcake/api/controller/product/ProductController.java
+++ api/src/main/java/com/katsshura/cupcake/api/controller/product/ProductController.java
@@ -3,12 +3,12 @@ package com.katsshura.cupcake.api.controller.product;
 import com.katsshura.cupcake.core.dto.product.ProductDTO;
 import com.katsshura.cupcake.core.services.product.ProductService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -30,5 +30,15 @@ public class ProductController {
         final var result = this.productService.createProduct(productDTO);
 
         return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> getProducts(@RequestParam(required = false) final String search,
+                                                        @PageableDefault final Pageable pageable) {
+        return ResponseEntity.ok(this.productService.getProducts(search, pageable));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> findProductById(@PathVariable final Long id) {
+        return ResponseEntity.ok(this.productService.findProductById(id));
     }
 }
--- core/src/main/java/com/katsshura/cupcake/core/services/product/ProductService.java
+++ core/src/main/java/com/katsshura/cupcake/core/services/product/ProductService.java
@@ -1,10 +1,16 @@
 package com.katsshura.cupcake.core.services.product;
 
 import com.katsshura.cupcake.core.dto.product.ProductDTO;
+import com.katsshura.cupcake.core.exceptions.NotFoundException;
 import com.katsshura.cupcake.core.mapper.product.ProductMapper;
 import com.katsshura.cupcake.core.repositories.product.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -28,5 +34,34 @@ public class ProductService {
         log.debug("Product persisted with success! [{}]", result);
 
         return this.productMapper.toDTO(result);
+    }
+
+    public ProductDTO findProductById(final Long id) {
+        log.debug("Fetching product with id: [{}]", id);
+
+        final var result = this.productRepository.findById(id);
+
+        if (result.isEmpty()) {
+            log.debug("Product not found for id: [{}]", id);
+            throw new NotFoundException("product_id: " + id);
+        }
+
+        log.debug("Product with id: [{}] was found: [{}]", id, result.get());
+
+        return this.productMapper.toDTO(result.get());
+    }
+
+    public Page<ProductDTO> getProducts(final String search, final Pageable pageable) {
+        log.debug("Fetching products with search parameter: {}", search);
+
+        if (StringUtils.isEmpty(search) || StringUtils.isBlank(search)) {
+            final var result =  this
+                    .productRepository.findAllByOrderByPopularityDesc(pageable);
+            return result.map(this.productMapper::toDTO);
+        }
+
+        final var result = this.productRepository
+                .findAllByNameContainingIgnoreCaseOrderByPopularityDesc(search, pageable);
+        return result.map(this.productMapper::toDTO);
     }
 }
