32f908e599430b4a9e96ad10a5c5279ea4afe963
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2022-10-07 00:26:12 -0300

    implemented test

--- core/src/test/java/com/katsshura/cupcake/core/integration/highlight/HighlightRepositoryTest.java
+++ core/src/test/java/com/katsshura/cupcake/core/integration/highlight/HighlightRepositoryTest.java
@@ -4,7 +4,6 @@ import com.katsshura.cupcake.core.config.IntegrationTestsConfiguration;
 import com.katsshura.cupcake.core.entities.highlight.HighlightEntity;
 import com.katsshura.cupcake.core.entities.product.ProductEntity;
 import com.katsshura.cupcake.core.repositories.highlight.HighlightRepository;
-import com.katsshura.cupcake.core.repositories.product.ProductRepository;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +13,20 @@ import org.springframework.test.context.jdbc.SqlGroup;
 import javax.transaction.Transactional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @IntegrationTestsConfiguration
 public class HighlightRepositoryTest {
 
     @Autowired
     private HighlightRepository highlightRepository;
-
-    @Autowired
-    private ProductRepository productRepository;
 
     @Nested
     @IntegrationTestsConfiguration
@@ -57,7 +59,29 @@ public class HighlightRepositoryTest {
         }
     }
 
+    @Nested
+    @IntegrationTestsConfiguration
     class ListHighlight {
+        @Test
+        @Transactional
+        @SqlGroup({
+                @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+                        scripts = "classpath:scripts/highlight/BeforeHighlightRepositoryTest.sql"),
+                @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+                        scripts = "classpath:scripts/highlight/AfterHighlightRepositoryTest.sql")
+        })
+        void shouldFindEnabled() {
+            final var result = highlightRepository.findFirstByEnabledIsTrue();
 
+            assertAll(
+                    () -> assertNotNull(result),
+                    () -> assertFalse(result.isEmpty()),
+                    () -> assertEquals(20, result.get().getId()),
+                    () -> assertEquals("Test Two", result.get().getTitle()),
+                    () -> assertEquals("Test Description Two", result.get().getSubtitle()),
+                    () -> assertEquals(20, result.get().getProduct_id()),
+                    () -> assertTrue(result.get().getEnabled())
+            );
+        }
     }
 }
